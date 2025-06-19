82535bfe9f72401ce1375e03a941ef2f81409baf
samishken <samishken@gmail.com>
2025-06-18 22:54:04 -0400

    update infra workflow

--- tf-src/common/variables.tf
+++ tf-src/common/variables.tf
@@ -56,12 +56,6 @@ variable "state_bucket" {
 
 }
 
-# variable "vpc_cidr" {
-#   description = "CIDR block for VPC"
-#   type        = string
-#   default     = "10.0.0.0/16"
-# }
-
 variable "private_subnets_cidrs" {
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
@@ -72,8 +66,3 @@ variable "public_subnets_cidrs" {
 variable "azs" {
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
-
-# variable "env" {
-#   description = "Environment (e.g., dev, prod)"
-#   type        = string
-# }
